d201251741f98f142f1ebfcdb6c34a575ec82987
k.garczewski <garczewskikrzysztof@gmail.com>
2023-02-20 16:01:54 +0100

    updated entrypoint

--- Dockerfile
+++ Dockerfile
@@ -6,9 +6,11 @@ WORKDIR /app
 COPY requirements.txt /app/
 RUN pip3 install -r requirements.txt
 RUN pip3 install psycopg2-binary
-
 COPY . /app/
 COPY entrypoint.sh /app/entrypoint.sh
+
+
+RUN sed -i "s/INSTALLED_APPS = \[/INSTALLED_APPS = \['django.contrib.sites',/" /app/ImageUploadAPI/settings.py
 
 RUN chmod +x /app/entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 
 # Apply other migrations
 python manage.py migrate ImageUploader 0001_initial
-python manage.py migrate ImageUploader 0002_auto_20230220_1530
-python manage.py migrate ImageUploader 0003_auto_20230220_1531
+python manage.py migrate ImageUploader 0002_auto_20230220_1554
+python manage.py migrate ImageUploader 0003_auto_20230220_1554
 
 # Start the server
 python manage.py runserver 0.0.0.0:8000
