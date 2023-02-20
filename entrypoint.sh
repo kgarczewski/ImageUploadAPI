637e4f8ef8864609fb5239c4b86713ae2ae24002
k.garczewski <garczewskikrzysztof@gmail.com>
2023-02-20 14:37:24 +0100

    entrypoint updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# Apply initial migrations
-python manage.py migrate
-
 # Apply other migrations
 python manage.py migrate ImageUploader 0001_initial
 python manage.py migrate ImageUploader 0002_auto_20230220_1123
