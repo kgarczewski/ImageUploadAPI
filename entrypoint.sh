9f6aa8b139fb0d5b56039322a88a506b1b905edb
k.garczewski <garczewskikrzysztof@gmail.com>
2023-02-20 19:16:43 +0100

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,8 @@
 python manage.py migrate ImageUploader 0001_initial
 python manage.py migrate ImageUploader 0002_auto_20230220_1810
 
+# Create the superuser with default account tier
+python manage.py create_superuser --username admin123 --email a@a.com --password admin --account_tier "Basic"
+
 # Start the server
 python manage.py runserver 0.0.0.0:8000
